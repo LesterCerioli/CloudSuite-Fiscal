a4e61d88fe5700b1d6643f8c115024e57e986447
Igor Moreira <igor.moreira@lucastechnologyservice.com>
2023-11-08 08:12:01 -0300

    "feat(#1079): updating exceptions into domain project"

--- src/Modules/CloudSuite.Modules.Domain/Exceptions/ApplicationException.cs
+++ src/Modules/CloudSuite.Modules.Domain/Exceptions/ApplicationException.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CloudSuite.Modules.Domain.Exceptions
+﻿namespace CloudSuite.Modules.Domain.Exceptions
 {
     public abstract class ApplicationException : Exception
     {
