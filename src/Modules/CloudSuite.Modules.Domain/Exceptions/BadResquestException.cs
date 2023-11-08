f4a50c4bbae7f4812e2fc857afac80e0f87b68e5
Igor Moreira <igor.moreira@lucastechnologyservice.com>
2023-11-08 08:08:01 -0300

    "feat(#1079): building exceptions into domain project"

---
+++ src/Modules/CloudSuite.Modules.Domain/Exceptions/BadResquestException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSuite.Modules.Domain.Exceptions
+{
+    public abstract class BadResquestException : ApplicationException
+    {
+        protected BadResquestException(string message)
+            : base("Bad Request", message)
+        {
+        }
+
+    }
+}
