4a0be476d11cd082ac74361ce75b3cda8d25e198
LesterCerioli <lesterlucasit@hotmail.com>
2023-11-16 09:27:05 -0500

    feat(#1187): Building DAS Domain Contract

--- src/Modules/CloudSuite.Modules.Domain/Contracts/IDASRepository.cs
+++ src/Modules/CloudSuite.Modules.Domain/Contracts/IDASRepository.cs
@@ -1,7 +1,24 @@
+using CloudSuite.Modules.Domain.Models;
+
 namespace CloudSuite.Modules.Domain.Contracts
 {
     public interface IDASRepository
     {
-         
+
+        Task<DAS> GetByReferenceMonth(string referenceMonth);
+
+        Task<DAS> GetByDueDate(DateTime dueDate);
+        
+        Task<DAS> GetByDocumentNumber(string documentNumber);
+
+        Task<DAS> GetByReferenceYear(string referenceYear);
+        
+        Task<IEnumerable<DAS>> GetList();
+
+        Task Add(DAS das);
+
+        void Update(DAS das)         ;
+
+        void Remove(DAS das);
     }
 }
