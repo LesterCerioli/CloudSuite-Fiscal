aa3f6a4e9fbd69ec880b7cc7acd56bc3e36c8be3
LesterCerioli <lesterlucasit@hotmail.com>
2023-11-16 08:52:41 -0500

    feat(#1187): Building Darf Domain Contract

--- src/Modules/CloudSuite.Modules.Domain/Contracts/IDarfRepository.cs
+++ src/Modules/CloudSuite.Modules.Domain/Contracts/IDarfRepository.cs
@@ -1,7 +1,24 @@
+using System.Data;
+using CloudSuite.Modules.Domain.Models;
+
 namespace CloudSuite.Modules.Domain.Contracts
 {
     public interface IDarfRepository
     {
-         
+        Task<Darf> GetByReferenceMonth(string referenceMonth);
+
+        Task<Darf> GetByDueDate(DateTime duedate) ;
+
+        Task<Darf> GetByDocumentNumber(string documentNumber);
+
+        Task<Darf> GetByValidationDate(DateTime validationDate);
+
+        Task<IEnumerable<Darf>> GetList();
+
+        Task Add(Darf darf);
+
+        void Update(Darf darf);
+
+        void Remove(Darf darf);
     }
 }
