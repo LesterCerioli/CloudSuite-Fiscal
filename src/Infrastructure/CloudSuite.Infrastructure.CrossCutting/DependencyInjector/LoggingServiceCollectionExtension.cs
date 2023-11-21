61d00ace40e4f836c9d90f773162e7790757d437
Lester Cerioli <lester.cerioli@lucastechnologyservices.onmicrosoft.com>
2023-11-21 19:14:32 -0300

    feat(#1269): Building CrossCutting Project

---
+++ src/Infrastructure/CloudSuite.Infrastructure.CrossCutting/DependencyInjector/LoggingServiceCollectionExtension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSuite.Infrastructure.CrossCutting.DependencyInjector
+{
+	public static class LoggingServiceCollectionExtension
+	{
+		public static IServiceCollection AddLogger(this IServiceCollection services)
+		{
+			var logger = LoggerManager.CreateLogger();
+			services.AddSingleton(logger);
+			return services;
+		}
+	}
+}
---
+++ src/Infrastructure/CloudSuite.Infrastructure.CrossCutting/NativeInjectorBootStrapper.cs
@@ -0,0 +1,38 @@
+﻿using CloudSuite.Infrastructure.Context;
+using CloudSuite.Modules.Domain.Contracts;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSuite.Infrastructure.CrossCutting
+{
+	public class NativeInjectorBootStrapper
+	{
+		public static void RegisterServices(IServiceCollection services)
+		{
+			// Infrastructure
+			services.AddScoped<IAddressRepository>();
+			services.AddScoped<ICancelOrderRepository>();
+			services.AddScoped<ICityRepository>();
+			services.AddScoped<ICountryRepository>();
+			services.AddScoped<IDarfRepository>();
+			services.AddScoped<IDASRepository>();
+			services.AddScoped<IDeclaracaoIRRepository>();
+			services.AddScoped<IDistrictRepository>();
+			services.AddScoped<IFederalTaxRepository>();
+			services.AddScoped<IIdeCancelamentoRepository>();
+			services.AddScoped<INoteRepository>();
+			services.AddScoped<IPrestadorRepository>();
+			services.AddScoped<IStateRepository>();
+			services.AddScoped<ITomadorServicoRepository>();
+			services.AddScoped<FiscalDbContext>();
+
+			// Application
+			//services.AddScoped<IAddressAppService, AddressAppService>();
+
+		}
+	}
+}
