50a3283e2100f9f044f11ba78a72a2626838a752
Igor Moreira <igor.moreira@lucastechnologyservice.com>
2023-11-23 13:33:31 -0300

    "feat(#1188): correcting handler"

--- src/Modules/CloudSuite.Modules.Application/Handlers/DeclaracaoIR/Requests/CheckDeclaracaoIRExistsByAlimonyRequest.cs
+++ src/Modules/CloudSuite.Modules.Application/Handlers/DeclaracaoIR/Requests/CheckDeclaracaoIRExistsByAlimonyRequest.cs
@@ -13,7 +13,7 @@ namespace CloudSuite.Modules.Application.Handlers.DeclaracaoIR.Requests
        
         public Guid Id { get; private set; }
 
-        public decimal? Alimony { get; private set; }
+        public decimal Alimony { get; private set; }
 
         public CheckDeclaracaoIRExistsByAlimonyRequest(decimal alimony)
         {
